69b76e94eae6b1b9178224b791d6725b80a8615f
Dave Jones <38425925+davejones74@users.noreply.github.com>
2020-03-24 17:41:30 +0000

    Update to add common tags required for db

--- main.tf
+++ main.tf
@@ -2,3 +2,9 @@ resource "azurerm_resource_group" "rg" {
   name     = "${var.product}-${var.env}"
   location = "${var.location}"
 }
+
+locals {
+  tags = "${merge(var.common_tags,
+    map("Team Contact", "#rpe")
+    )}"
+}
