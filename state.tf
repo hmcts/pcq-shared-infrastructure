197603c0d63a0ceaf4acd07643d9b65326e55eee
Bradley Wadsworth <bradley.wadsworth@hmcts.net>
2020-09-09 16:01:52 +0100

    Remove required parameters

--- state.tf
+++ state.tf
@@ -1,12 +1,5 @@
 terraform {
   backend "azurerm" {}
-
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~= 2.25"
-    }
-  }
 }
 
 data "azurerm_subnet" "subnet_a" {
