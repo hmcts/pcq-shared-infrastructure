bfe451bedfe5aa11b7a6cbf414acf1c3756c510a
Dave Jones <dave.jones@hmcts.net>
2020-09-18 14:40:44 +0100

    PCQ-653 Adding account storage scripts to shared-infra.

--- key-vault.tf
+++ key-vault.tf
@@ -1,5 +1,5 @@
 module "pcq-vault" {
-  source                     = "git@github.com:hmcts/cnp-module-key-vault?ref=azurermv2"
+  source                     = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
   name                       = "pcq-${var.env}"
   product                    = "${var.product}"
   env                        = "${var.env}"
