c2ef03e6739a98d122cd8cd67ff2e02a13b6fd9d
Bradley Wadsworth <bradley.wadsworth@hmcts.net>
2020-09-25 14:46:40 +0100

    Remove quoted references

--- storage-account.tf
+++ storage-account.tf
@@ -36,67 +36,67 @@ module "pcq_storage_account" {
 }
 
 data "azurerm_virtual_network" "mgmt_vnet" {
-  provider            = "azurerm.mgmt"
+  provider            = azurerm.mgmt
   name                = local.mgmt_network_name
   resource_group_name = local.mgmt_network_rg_name
 }
 
 data "azurerm_subnet" "jenkins_subnet" {
-  provider             = "azurerm.mgmt"
+  provider             = azurerm.mgmt
   name                 = "iaas"
   virtual_network_name = data.azurerm_virtual_network.mgmt_vnet.name
   resource_group_name  = data.azurerm_virtual_network.mgmt_vnet.resource_group_name
 }
 
 data "azurerm_subnet" "aks-00-mgmt" {
-  provider             = "azurerm.mgmt"
+  provider             = azurerm.mgmt
   name                 = "aks-00"
   virtual_network_name = data.azurerm_virtual_network.mgmt_vnet.name
   resource_group_name  = data.azurerm_virtual_network.mgmt_vnet.resource_group_name
 }
 
 data "azurerm_subnet" "aks-01-mgmt" {
-  provider             = "azurerm.mgmt"
+  provider             = azurerm.mgmt
   name                 = "aks-01"
   virtual_network_name = data.azurerm_virtual_network.mgmt_vnet.name
   resource_group_name  = data.azurerm_virtual_network.mgmt_vnet.resource_group_name
 }
 
 data "azurerm_virtual_network" "aks_core_vnet" {
-  provider            = "azurerm.aks-infra"
+  provider            = azurerm.aks-infra
   name                = "core-${var.env}-vnet"
   resource_group_name = "aks-infra-${var.env}-rg"
 }
 
 data "azurerm_subnet" "aks-00-infra" {
-  provider             = "azurerm.aks-infra"
+  provider             = azurerm.aks-infra
   name                 = "aks-00"
   virtual_network_name = data.azurerm_virtual_network.aks_core_vnet.name
   resource_group_name  = data.azurerm_virtual_network.aks_core_vnet.resource_group_name
 }
 
 data "azurerm_subnet" "aks-01-infra" {
-  provider             = "azurerm.aks-infra"
+  provider             = azurerm.aks-infra
   name                 = "aks-01"
   virtual_network_name = data.azurerm_virtual_network.aks_core_vnet.name
   resource_group_name  = data.azurerm_virtual_network.aks_core_vnet.resource_group_name
 }
 
 data "azurerm_virtual_network" "aks_preview_vnet" {
-  provider            = "azurerm.aks-preview"
+  provider            = azurerm.aks-preview
   name                = "core-preview-vnet"
   resource_group_name = "aks-infra-preview-rg"
 }
 
 data "azurerm_subnet" "aks-00-preview" {
-  provider             = "azurerm.aks-preview"
+  provider             = azurerm.aks-preview
   name                 = "aks-00"
   virtual_network_name = data.azurerm_virtual_network.aks_preview_vnet.name
   resource_group_name  = data.azurerm_virtual_network.aks_preview_vnet.resource_group_name
 }
 
 data "azurerm_subnet" "aks-01-preview" {
-  provider             = "azurerm.aks-preview"
+  provider             = azurerm.aks-preview
   name                 = "aks-01"
   virtual_network_name = data.azurerm_virtual_network.aks_preview_vnet.name
   resource_group_name  = data.azurerm_virtual_network.aks_preview_vnet.resource_group_name
