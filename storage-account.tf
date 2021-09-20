9019d8b27e9f8036786c25d840d6ee51742a43cd
Priyanka Verma <priyanka.verma@hmcts.net>
2021-09-20 12:27:53 +0100

    PCQ-1071UpdateVnetSubnet

--- storage-account.tf
+++ storage-account.tf
@@ -3,11 +3,8 @@ locals {
   mgmt_network_name         = "core-cftptl-intsvc-vnet"
   mgmt_network_rg_name      = "aks-infra-cftptl-intsvc-rg"
 
-  cft_vnet_name = "cft-${var_env}-vnet"
-  cft_vnet_resource_group = "cft-${var_env}-network-rg"
-
-  vnet_name = var.env == "sbox" || var.env == "perftest" || var.env == "aat" || var.env == "ithc" ? local.cft_vnet_name : "core-${var.env}-vnet"
-  vnet_resource_group_name = var.env == "sbox" || var.env == "perftest" || var.env == "aat" || var.env == "ithc" ? local.cft_vnet_name : "aks-infra-${var.env}-rg"
+  vnet_name    = var.env == "sbox" || var.env == "perftest" || var.env == "aat" || var.env == "ithc" ? "cft-${var.env}-vnet" : "core-${var.env}-vnet"
+  vnet_resource_group_name = var.env == "sbox" || var.env == "perftest" || var.env == "aat" || var.env == "ithc" ? "cft-${var.env}-network-rg" : "aks-infra-${var.env}-rg"
 
   sa_aat_subnets = [
     data.azurerm_subnet.jenkins_subnet.id,
