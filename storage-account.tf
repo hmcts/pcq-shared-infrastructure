240238e0cb6153c45f819a0c0f2459647b14e9fd
Ciarán Carragher <c.carragher@kainos.com>
2022-03-07 21:31:31 +0000

    Rework some aspects of code to add in both sets of prod subnets

--- storage-account.tf
+++ storage-account.tf
@@ -1,60 +1,44 @@
 locals {
-  storage_account_name      = "${var.product}shared${var.env}"
-  mgmt_network_name         = "cft-ptl-vnet"
-  mgmt_network_rg_name      = "cft-ptl-network-rg"
+  storage_account_name         = "${var.product}shared${var.env}"
+  mgmt_network_name            = "cft-ptl-vnet"
+  mgmt_network_rg_name         = "cft-ptl-network-rg"
 
-  vnet_name = var.env == "sbox" || var.env == "perftest" || var.env == "aat" || var.env == "ithc" || var.env == "preview" ? "cft-${var.env}-vnet" : "core-${var.env}-vnet"
+  vnet_name                = var.env == "sbox" || var.env == "perftest" || var.env == "aat" || var.env == "ithc" || var.env == "preview" ? "cft-${var.env}-vnet" : "core-${var.env}-vnet"
   vnet_resource_group_name = var.env == "sbox" || var.env == "perftest" || var.env == "aat" || var.env == "ithc" || var.env == "preview" ? "cft-${var.env}-network-rg" : "aks-infra-${var.env}-rg"
 
-  sa_aat_subnets = [
+  standard_subnets = [
     data.azurerm_subnet.jenkins_subnet.id,
     data.azurerm_subnet.aks-00-mgmt.id,
     data.azurerm_subnet.aks-01-mgmt.id,
     data.azurerm_subnet.aks-00-infra.id,
-    data.azurerm_subnet.aks-01-infra.id,
-    data.azurerm_subnet.aks-00-preview.id,
-    data.azurerm_subnet.aks-01-preview.id]
-  
-  sa_prod_subnets = [
-    data.azurerm_subnet.jenkins_subnet.id,
-    data.azurerm_subnet.aks-00-mgmt.id,
-    data.azurerm_subnet.aks-01-mgmt.id,
-    data.azurerm_subnet.aks-00-infra.id,
-    data.azurerm_subnet.aks-01-infra.id,
-    data.azurerm_subnet.aks-00-prod.id,
-    data.azurerm_subnet.aks-01-prod.id]
+    data.azurerm_subnet.aks-01-infra.id
+  ]
 
-
-  sa_other_subnets = [
-    data.azurerm_subnet.jenkins_subnet.id,
-    data.azurerm_subnet.aks-00-mgmt.id,
-    data.azurerm_subnet.aks-01-mgmt.id,
-    data.azurerm_subnet.aks-00-infra.id,
-    data.azurerm_subnet.aks-01-infra.id]
-
-  sa_subnets = split(",", var.env == "aat" ? join(",", local.sa_aat_subnets) : join(",", local.sa_other_subnets) || var.env == "prod" ? join(",", local.sa_prod_subnets): join(",", local.sa_other_subnets))
+  preview_subnets  = var.env == "aat" ? [data.azurerm_subnet.aks-00-preview.id, data.azurerm_subnet.aks-00-preview.id] : []
+  cft_prod_subnets = var.env == "prod" ? [data.azurerm_subnet.aks-00-prod.id, data.azurerm_subnet.aks-01-prod-subnet.id] : []
+  sa_subnets       = concat(local.standard_subnets, local.preview_subnets, local.cft_prod_subnets)
 }
 
 // pcq blob Storage Account
 module "pcq_storage_account" {
-  source                    = "git@github.com:hmcts/cnp-module-storage-account?ref=master"
-  env                       = var.env
-  storage_account_name      = local.storage_account_name
-  resource_group_name       = azurerm_resource_group.rg.name
-  location                  = var.location
-  account_kind              = "StorageV2"
-  account_tier              = "Standard"
-  account_replication_type  = "ZRS"
-  access_tier               = "Hot"
+  source                   = "git@github.com:hmcts/cnp-module-storage-account?ref=master"
+  env                      = var.env
+  storage_account_name     = local.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = var.location
+  account_kind             = "StorageV2"
+  account_tier             = "Standard"
+  account_replication_type = "ZRS"
+  access_tier              = "Hot"
 
   // enable_blob_encryption    = true
   // enable_file_encryption    = true
   enable_https_traffic_only = true
 
   // Tags
-  common_tags               = var.common_tags
-  team_contact              = var.team_contact
-  destroy_me                = var.destroy_me
+  common_tags  = var.common_tags
+  team_contact = var.team_contact
+  destroy_me   = var.destroy_me
 
   sa_subnets = local.sa_subnets
 }
@@ -66,13 +50,13 @@ resource "azurerm_storage_management_policy" "pcq_lifecycle_rules" {
     name    = "pcqExpirationRule"
     enabled = true
     filters {
-      blob_types   = ["blockBlob"]
+      blob_types = ["blockBlob"]
     }
     actions {
       base_blob {
         tier_to_archive_after_days_since_modification_greater_than = 90
-        tier_to_cool_after_days_since_modification_greater_than = 90
-        delete_after_days_since_modification_greater_than = 90
+        tier_to_cool_after_days_since_modification_greater_than    = 90
+        delete_after_days_since_modification_greater_than          = 90
       }
     }
   }
@@ -125,6 +109,26 @@ data "azurerm_subnet" "aks-01-infra" {
   resource_group_name  = data.azurerm_virtual_network.aks_core_vnet.resource_group_name
 }
 
+data "azurerm_virtual_network" "aks_prod_vnet" {
+  provider            = azurerm.aks-infra
+  name                = "cft-prod-vnet"
+  resource_group_name = "cft-prod-network-rg"
+}
+
+data "azurerm_subnet" "aks-00-prod" {
+  provider             = azurerm.aks-infra
+  name                 = "aks-00"
+  virtual_network_name = data.azurerm_virtual_network.aks_prod_vnet.name
+  resource_group_name  = data.azurerm_virtual_network.aks_prod_vnet.resource_group_name
+}
+
+data "azurerm_subnet" "aks-01-prod" {
+  provider             = azurerm.aks-prod
+  name                 = "aks-01"
+  virtual_network_name = data.azurerm_virtual_network.aks_prod_vnet.name
+  resource_group_name  = data.azurerm_virtual_network.aks_prod_vnet.resource_group_name
+}
+
 data "azurerm_virtual_network" "aks_preview_vnet" {
   provider            = azurerm.aks-preview
   name                = "cft-preview-vnet"
@@ -166,44 +170,44 @@ data "azurerm_subnet" "aks-01-prod" {
 }
 
 resource "azurerm_storage_container" "pcq_containers" {
-  name                 = "pcq"
-  storage_account_name = module.pcq_storage_account.storageaccount_name
+  name                  = "pcq"
+  storage_account_name  = module.pcq_storage_account.storageaccount_name
   container_access_type = "private"
 }
 
 resource "azurerm_storage_container" "pcq_rejected_container" {
-  name                 = "pcq-rejected"
-  storage_account_name = module.pcq_storage_account.storageaccount_name
+  name                  = "pcq-rejected"
+  storage_account_name  = module.pcq_storage_account.storageaccount_name
   container_access_type = "private"
 }
 
 // pcq blob Storage Account Vault Secrets
 resource "azurerm_key_vault_secret" "storage_account_name" {
-  name      = "pcq-storage-account-name"
-  value     = module.pcq_storage_account.storageaccount_name
+  name         = "pcq-storage-account-name"
+  value        = module.pcq_storage_account.storageaccount_name
   key_vault_id = data.azurerm_key_vault.pcq_key_vault.id
 }
 
 resource "azurerm_key_vault_secret" "pcq_storageaccount_primary_access_key" {
-  name      = "pcq-storage-account-primary-access-key"
-  value     = module.pcq_storage_account.storageaccount_primary_access_key
+  name         = "pcq-storage-account-primary-access-key"
+  value        = module.pcq_storage_account.storageaccount_primary_access_key
   key_vault_id = data.azurerm_key_vault.pcq_key_vault.id
 }
 
 resource "azurerm_key_vault_secret" "pcq_storageaccount_secondary_access_key" {
-  name      = "pcq-storage-account-secondary-access-key"
-  value     = module.pcq_storage_account.storageaccount_secondary_access_key
+  name         = "pcq-storage-account-secondary-access-key"
+  value        = module.pcq_storage_account.storageaccount_secondary_access_key
   key_vault_id = data.azurerm_key_vault.pcq_key_vault.id
 }
 
 resource "azurerm_key_vault_secret" "pcq_storageaccount_primary_connection_string" {
-  name      = "pcq-storage-account-primary-connection-string"
-  value     = module.pcq_storage_account.storageaccount_primary_connection_string
+  name         = "pcq-storage-account-primary-connection-string"
+  value        = module.pcq_storage_account.storageaccount_primary_connection_string
   key_vault_id = data.azurerm_key_vault.pcq_key_vault.id
 }
 
 resource "azurerm_key_vault_secret" "pcq_storageaccount_secondary_connection_string" {
-  name      = "pcq-storage-account-secondary-connection-string"
-  value     = module.pcq_storage_account.storageaccount_secondary_connection_string
+  name         = "pcq-storage-account-secondary-connection-string"
+  value        = module.pcq_storage_account.storageaccount_secondary_connection_string
   key_vault_id = data.azurerm_key_vault.pcq_key_vault.id
 }
