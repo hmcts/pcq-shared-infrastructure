6b4fc1c2596f91faf0ba6223718c9bd9ec10c24b
54687 <61242337+hmcts-jenkins-j-to-z[bot]@users.noreply.github.com>
2024-07-04 06:42:18 +0000

    Updating Terraform Formatting

--- key-vault.tf
+++ key-vault.tf
@@ -1,16 +1,16 @@
 module "pcq-vault" {
-  source                     = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
-  name                       = "pcq-${var.env}"
-  product                    = var.product
-  env                        = var.env
-  tenant_id                  = var.tenant_id
-  object_id                  = var.jenkins_AAD_objectId
-  resource_group_name        = azurerm_resource_group.rg.name
+  source              = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
+  name                = "pcq-${var.env}"
+  product             = var.product
+  env                 = var.env
+  tenant_id           = var.tenant_id
+  object_id           = var.jenkins_AAD_objectId
+  resource_group_name = azurerm_resource_group.rg.name
 
   # dcd_group_pcq_v2 group object ID
-  product_group_object_id    = "731343b8-be79-4a97-b14e-60be786ad393"
-  common_tags                = var.common_tags
-  create_managed_identity    = true
+  product_group_object_id = "731343b8-be79-4a97-b14e-60be786ad393"
+  common_tags             = var.common_tags
+  create_managed_identity = true
 }
 
 data "azurerm_key_vault" "pcq_key_vault" {
--- perftest.tfvars
+++ perftest.tfvars
@@ -1,2 +1,2 @@
-aks_infra_subscription_id = "8a07fdcd-6abd-48b3-ad88-ff737a4b9e3c"
+aks_infra_subscription_id   = "8a07fdcd-6abd-48b3-ad88-ff737a4b9e3c"
 aks_preview_subscription_id = "8b6ea922-0862-443e-af15-6056e1c9b9a4"
